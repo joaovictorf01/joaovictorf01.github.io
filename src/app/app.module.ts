22fa1fe2aabcbf504a0d8f5210d93b61e4e7c397
João Victor <joaovictorf01@gmail.com>
2023-12-29 08:29:14 -0300

    Update date format and dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { FriendDetailComponent } from './friend-detail/friend-detail.component';
 import { FriendEditComponent } from './friend-edit/friend-edit.component';
 import { FriendAddComponent } from './friend-add/friend-add.component';
 import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
+
 
 @NgModule({
   declarations: [
@@ -20,9 +25,13 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+
   ],
-  providers: [],
+  providers: [
+    { provide: 'pt-BR', useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
